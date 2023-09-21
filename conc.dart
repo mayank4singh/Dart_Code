8f63ea8889bb996e5b9ff6657ae87efc53c947b9
mayank4singh <mailatmausam@gmail.com>
2023-09-21 11:18:44 +0530

    Initial Commit

---
+++ conc.dart
@@ -0,0 +1,6 @@
+void main() {
+  String firstName = "Rahul";
+  String lastName = "Pandey";
+  print("Using +, Full Name is " + firstName + " " + lastName + ".");
+  print("Using interpolation, full name is $firstName $lastName.");
+}
