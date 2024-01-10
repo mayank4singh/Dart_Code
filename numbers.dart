22b239a962cfe3c2063320fd9820bfb9a65e0b07
mayank4singh <mailatmausam@gmail.com>
2024-01-10 18:16:26 +0530

    Initial Commit

---
+++ numbers.dart
@@ -0,0 +1,9 @@
+void main() {
+  num num1 = 10;
+  num num2 = 3;
+
+  double div = num1 / num2;
+  print("The division is ${div.toStringAsFixed(3)}");
+
+  ///toStringAsFixed is used to limit the decimal points upto some digits.
+}
---
+++ string.dart
@@ -0,0 +1,7 @@
+void main() {
+  String name = 'Mayank Singh';
+  String place = "Prayagraj";
+  print("My name is $name. \nI am from $place");
+
+  ///Dart accepts both single and double quotes as string inputs.
+}
