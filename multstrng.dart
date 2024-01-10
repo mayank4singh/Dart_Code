c8f65ae068c18bb13a54cff2fd5f1748ad0498fa
mayank4singh <mailatmausam@gmail.com>
2024-01-10 18:30:56 +0530

    Initial Commit

---
+++ multstrng.dart
@@ -0,0 +1,7 @@
+void main() {
+  String details =
+      """Hello this is developer Mayank learning Dart programming language. 
+         If you visited my profile do follow and star this repo.
+         Basically this is Dart multiline string.""";
+  print(details);
+}
