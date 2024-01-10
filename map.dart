bf4b53c72d87743f5336b67ce3f2bf6464cf6799
mayank4singh <mailatmausam@gmail.com>
2024-01-10 22:04:09 +0530

    Initial Commit

---
+++ map.dart
@@ -0,0 +1,10 @@
+void main() {
+  Map<String, String> details = {
+    "Name": "Mayank Singh",
+    "Age": "20",
+    "Domain": "Developer"
+  };
+  print(details["Name"]);
+  print(details["Age"]);
+  print(details["Domain"]);
+}
