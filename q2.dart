0d150b9d623dd067343010f38a8e3f8f52a3242d
mayank4singh <mailatmausam@gmail.com>
2024-01-30 21:19:44 +0530

    Initial Commit

---
+++ q2.dart
@@ -0,0 +1,10 @@
+//Write a program to print Hello I am “John Doe” and Hello I’am “John Doe” with single and double quotes.
+void main() {
+  String name = "John Doe";
+
+  // Using double quotes
+  print('Hello, I am "$name"');
+
+  // Using single quotes
+  print('Hello, I\'am "$name"');
+}
