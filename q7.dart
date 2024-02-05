cdf3f8593ff4fa636fc3b8f36dc1394d92ccfefb
mayank4singh <mailatmausam@gmail.com>
2024-02-05 20:06:59 +0530

    Initial Commit

---
+++ q7.dart
@@ -0,0 +1,14 @@
+//Write a program to find quotient and remainder of two integers.
+void main() {
+  int dividend = 25;
+  int divisor = 4;
+
+  int quotient = dividend ~/ divisor;
+
+  int remainder = dividend % divisor;
+
+  print('Dividend: $dividend');
+  print('Divisor: $divisor');
+  print('Quotient: $quotient');
+  print('Remainder: $remainder');
+}
